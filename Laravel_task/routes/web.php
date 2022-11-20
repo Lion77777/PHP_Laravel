0cc85a7837d93b6362723088c5b7ca54b70b071a
Lion77777 <leon.avetisyan.1993@gmail.com>
2022-11-21 01:45:18 +0400

    Form routes were made

--- Laravel_task/routes/web.php
+++ Laravel_task/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FormValidationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/form', [FormValidationController::class, 'createUserForm']);
+Route::post('/form', [FormValidationController::class, 'UserForm'])->name('validate.form');
