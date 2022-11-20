ce6e29918f12aba755056251b65a8aac9d75e1b9
Lion77777 <leon.avetisyan.1993@gmail.com>
2022-11-21 00:43:08 +0400

    Database migration was done

--- Laravel_task/app/Models/Form.php
+++ Laravel_task/app/Models/Form.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Form extends Model
 {
     use HasFactory;
+
+    public $fillable = [
+        'name',
+        'email',
+        'message',
+    ];
 }
